a44b257b0dddba0ab6655d6b36d887ae6dd0cb14
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-26 17:58:10 +0530

    modified

--- apicontroller/block.js
+++ apicontroller/block.js
@@ -109,7 +109,6 @@ async function readBlockById(req, res) {
 }
 
 async function readBlockFloorBlockCodeCount(req, res) { 
-    console.log(req.session.isLogged, 'trueeeeeeeeeeeeeeeeeeee')
     const mysqlClient = req.app.mysqlClient;
     const blockId = req.query.blockId;
     try {
