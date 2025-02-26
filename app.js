c3726878bb399b6b27e9f0a3936e66a6e400a99d
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-26 11:59:19 +0530

    app modified

--- app.js
+++ app.js
@@ -40,18 +40,6 @@ const sessionStore = new MySQLStore(dbOptions)
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(cookieParser())
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://hostelbackend-production-24da.up.railway.app'); // Allow frontend
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-  });
   
 const corsOptions = {
     origin: ['https://hostelbackend-production-24da.up.railway.app' ],  // Allow specific origin 
