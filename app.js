250b4377a4149b4a20999557f706c88300213590
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-26 17:50:09 +0530

    modified

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24,  
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         httpOnly: true
     }
 }))
