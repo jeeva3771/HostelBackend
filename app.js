f0d2be9dd80cd12d720aad15a485bc7981b97d96
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-26 10:54:20 +0530

    app modified

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(cookieParser())
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://hostelbackend-production-24da.up.railway.app'); // Allow frontend
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+  
 const corsOptions = {
     origin: ['https://hostelbackend-production-24da.up.railway.app' ],  // Allow specific origin 
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
