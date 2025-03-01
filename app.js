6c0346bb0bb2fe2b486f1f4320dcd962d1a6e5a8
mr sivasankar <sivasankar.k@creativebees.in>
2025-03-01 17:52:16 +0530

    added

--- app.js
+++ app.js
@@ -125,32 +125,32 @@ const studentUrls = [
     '/student/api/student/editimage'
 ]
 
-app.use((req, res, next) => {
-    if (pageWardenSessionExclude.includes(req.originalUrl)) {
-        return next()
-    }
+// app.use((req, res, next) => {
+//     if (pageWardenSessionExclude.includes(req.originalUrl)) {
+//         return next()
+//     }
 
-    if (req.originalUrl !== '/login') {
-        if (req.session.isLogged !== true) {
-            return res.status(401).send('Session expired.')
-        }
-    }
-    return next()
-})
+//     if (req.originalUrl !== '/login') {
+//         if (req.session.isLogged !== true) {
+//             return res.status(401).send('Session expired.')
+//         }
+//     }
+//     return next()
+// })
 
-app.use((req, res, next) => {
-    if (pageStudentSessionExclude.includes(req.originalUrl)) {
-        return next()
-    }
+// app.use((req, res, next) => {
+//     if (pageStudentSessionExclude.includes(req.originalUrl)) {
+//         return next()
+//     }
 
-    if (req.originalUrl !== '/student/login') {
-        if (studentUrls.includes(req.originalUrl) && req.session.isLoggedStudent !== true) {
-            return res.status(401).send('Session expired.');
-        }
-    }
+//     if (req.originalUrl !== '/student/login') {
+//         if (studentUrls.includes(req.originalUrl) && req.session.isLoggedStudent !== true) {
+//             return res.status(401).send('Session expired.');
+//         }
+//     }
     
-    return next()
-})
+//     return next()
+// })
 
 app.mysqlClient.connect(function (err) {
     if (err) {
