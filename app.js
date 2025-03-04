c5ede463bd926f7c22b0da12631a05fa7ab81577
mr sivasankar <sivasankar.k@creativebees.in>
2025-03-04 11:45:35 +0530

    added'

--- app.js
+++ app.js
@@ -37,8 +37,8 @@ const corsOptions = {
     // origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
     credentials : true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['Set-Cookie']
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+    // exposedHeaders: ['Set-Cookie']
 }
 app.use(cors(corsOptions));
 
