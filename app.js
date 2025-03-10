4caacb7de3d9bf9f8fd089a60bc3ac2ad33b8eb8
jeeva3771 <jeeva37710@gmail.com>
2025-03-10 23:00:54 +0530

    added

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    console.log("Session ID:", req.sessionID);
+    console.log("Session ID:", req.session);
     next();
 });
 
